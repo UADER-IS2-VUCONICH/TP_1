928a8bddd52ac8f8d11c5780af12f2ee8d853d3f
UADER-IS2-VUCONICH <ianvuconich@gmail.com>
2024-03-25 23:14:42 -0300

    comentarios agregados

--- SRC/primos.py
+++ SRC/primos.py
@@ -1,4 +1,4 @@
-# Python program to display all the prime numbers within an interval
+# Programa de python para mostrar todos los numeros primos dentro de un intervalo
 
 lower = 1
 upper = 100
@@ -6,7 +6,7 @@ upper = 100
 print("Prime numbers between", lower, "and", upper, "are:")
 
 for num in range(lower, upper + 1):
-# all prime numbers are greater than 1
+# todos los numeros primos son mayores que 1
     if num > 1:
         for i in range(2, num):
             if (num % i) == 0:
